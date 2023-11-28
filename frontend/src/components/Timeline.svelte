0c76bc5c3409c2aa8fe5de7ef7e4588e1cf74776
k1nho <kinhong99@gmail.com>
2023-11-28 15:17:07 -0500

    🔨 fix: make timeline bar max width the same as track max width

--- frontend/src/components/Timeline.svelte
+++ frontend/src/components/Timeline.svelte
@@ -32,6 +32,20 @@
       .catch((e) => console.log(e));
   }
 
+  function calculateMaxTrackWidth() {
+    const trackElements = document.querySelectorAll(".video-track");
+    let maxWidth = 0;
+
+    trackElements.forEach((trackElement) => {
+      const trackWidth = trackElement.clientWidth;
+      if (trackWidth > maxWidth) {
+        maxWidth = trackWidth;
+      }
+    });
+
+    return maxWidth;
+  }
+
   onDestroy(() => {
     trackFiles.reset();
     hoverPos = 0;
@@ -40,7 +54,8 @@
 
 <div
   class="timeline h-full w-full bg-gdark border-t-2 border-t-white flex flex-col gap-4 pt-4 pb-4 relative"
-  on:mousemove={(e) => (hoverPos = e.clientX)}
+  on:mousemove={(e) =>
+    (hoverPos = Math.min(e.clientX, calculateMaxTrackWidth()))}
   use:dropzone={{}}
 >
   <div
@@ -51,7 +66,9 @@
   </div>
   <!-- VIDEO TRACKS -->
   {#each $trackFiles as track (track.filepath + track.name)}
-    <div class="w-1/4 h-28 bg-teal rounded-md border-gblue border-2">
+    <div
+      class="w-1/4 h-28 bg-teal rounded-md border-gblue border-2 video-track"
+    >
       <img
         src={loadThumbnail(track) + `?${timestamp}`}
         alt={`video: ${track.name}`}
